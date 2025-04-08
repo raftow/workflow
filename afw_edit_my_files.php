0d3aee4617f88b92ad747faf4905bce2e8d97efe
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-04-08 10:57:01 +0300

    Admission & Farz settings and simulator/manager

---
+++ afw_edit_my_files.php
@@ -0,0 +1,7 @@
+<?php
+     $file_dir_name = dirname(__FILE__);
+     include_once ("$file_dir_name/ini.php");
+     include_once ("$file_dir_name/module_config.php");
+      
+     include("$file_dir_name/../lib/afw/afw_edit_my_files.php");
+?>
