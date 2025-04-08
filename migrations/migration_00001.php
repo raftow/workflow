084f17249a50289c295d625b18e5818b38544cfd
Rafik Laptop2 BME <rafik.towati@gmail.com>
2025-04-08 13:49:46 +0300

    Admission & Farz settings and simulator/manager

--- migrations/migration_00001.php
+++ migrations/migration_00001.php
@@ -2,6 +2,8 @@
 if(!class_exists("AfwSession")) die("Denied access");
 $server_db_prefix = AfwSession::config("db_prefix", "default_db_");
 
+
+AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."workflow.`workflow_file` CHANGE `afile_size` `afile_size` INT(11) NULL DEFAULT NULL;");
 AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."workflow.workflow_file add   owner_type varchar(32)  DEFAULT NULL  AFTER original_name");
 AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."workflow.workflow_file add   owner_id int(11) DEFAULT NULL  AFTER owner_type");
 
