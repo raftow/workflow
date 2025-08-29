62a48c44853e6b78e3bc0b609f2aa5257f9b81f5
rafiq boubaker <rafik.towati@gmail.com>
2025-08-29 17:59:01 +0300

    work on uoh deployment from Tunis

--- afw_edit_my_files.php
+++ afw_edit_my_files.php
@@ -2,6 +2,12 @@
      $file_dir_name = dirname(__FILE__);
      include_once ("$file_dir_name/ini.php");
      include_once ("$file_dir_name/module_config.php");
-      
-     include("$file_dir_name/../lib/afw/afw_edit_my_files.php");
+
+     require_once ("$file_dir_name/../lib/afw/afw_autoloader.php");
+     AfwSession::startSession();
+     $objme = AfwSession::getUserConnected();
+     $me = "u".$objme->id;
+     $codeme = substr(md5("code".$me),0,8);
+
+     include("$file_dir_name/../lib/afw/afw_edit_my_files.php?y=$codeme");
 ?>
