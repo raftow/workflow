13f9297a958ab49826b9d5abae3d50af4fe09b3a
rafiq boubaker <rafik.towati@gmail.com>
2025-10-18 20:48:36 +0300

    work on nauss project

--- tr/trad_ar_workflow_scope.php
+++ tr/trad_ar_workflow_scope.php
@@ -1,23 +1,24 @@
 <?php
-class WorkflowScopeArTranslator{
+class WorkflowScopeArTranslator
+{
 
-    public static function initData()
-    {
-        $trad = [];
-
-	$trad["workflow_scope"]["workflowscope.single"] = "مجال";
-	$trad["workflow_scope"]["workflowscope.new"] = "جديد ة";
-	$trad["workflow_scope"]["workflow_scope"] = "المجالات";
-	$trad["workflow_scope"]["scope_name_ar"] = "اسم المجال - عربي";
-	$trad["workflow_scope"]["scope_name_en"] = "اسم المجال - انجليزي";
-	$trad["workflow_scope"]["scope_description_ar"] = "وصف المجال - عربي";
-	$trad["workflow_scope"]["scope_description_en"] = "وصف المجال - انجليزي";
-        return $trad;
-        }
-
-        public static function getInstance()
+	public static function initData()
 	{
-                if(false) return new WorkflowScopeEnTranslator();
+		$trad = [];
+
+		$trad["workflow_scope"]["workflowscope.single"] = "مجال";
+		$trad["workflow_scope"]["workflowscope.new"] = "جديد	";
+		$trad["workflow_scope"]["workflow_scope"] = "المجالات";
+		$trad["workflow_scope"]["scope_name_ar"] = "اسم المجال - عربي";
+		$trad["workflow_scope"]["scope_name_en"] = "اسم المجال - انجليزي";
+		$trad["workflow_scope"]["scope_description_ar"] = "وصف المجال - عربي";
+		$trad["workflow_scope"]["scope_description_en"] = "وصف المجال - انجليزي";
+		return $trad;
+	}
+
+	public static function getInstance()
+	{
+		if (false) return new WorkflowScopeEnTranslator();
 		return new WorkflowScope();
 	}
 }
