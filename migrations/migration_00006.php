bb5d3accb6ee8cef7cfc81ed09c8e9b671420240
Medali Maaouani <maaoini@gmail.com>
2025-10-22 12:24:08 +0300

    update sprint 2

--- migrations/migration_00006.php
+++ migrations/migration_00006.php
@@ -6,7 +6,7 @@ try
 {
   
     AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."workflow.workflow_stage change   standard_processing_time standard_processing_time smallint DEFAULT 0 AFTER required_application_field_mfk;");
-    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."workflow.workflow_stage change   processing_request_responsibility processing_request_responsibility smallint DEFAULT NULL  AFTER time_unit;");
+    AfwDatabase::db_query("ALTER TABLE ".$server_db_prefix."workflow.workflow_stage change   processing_request_responsibility processing_request_responsibility smallint DEFAULT 0  AFTER time_unit;");
 
 }
 catch(Exception $e)
