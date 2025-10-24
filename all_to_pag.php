9bcb2927ed1f9cfffca22b29aa292b2e0d39d6c5
rafiq boubaker <rafik.towati@gmail.com>
2025-10-24 16:57:50 +0300

    work on nauss project

---
+++ all_to_pag.php
@@ -0,0 +1,27 @@
+<?php
+
+if($modtopag != "workflow") die("check the module you want to pag");
+
+ $arr_all_files = array();
+
+ 
+ $arr_all_files[]="content";
+ $arr_all_files[]="content_item";
+ $arr_all_files[]="idn_to_id";
+ $arr_all_files[]="intelligent_content";
+ $arr_all_files[]="page";
+ $arr_all_files[]="page_item";
+ $arr_all_files[]="page_section";
+ $arr_all_files[]="page_theme";
+ $arr_all_files[]="publication";
+ $arr_all_files[]="section_template";
+ $arr_all_files[]="workflow_commitee";
+ $arr_all_files[]="workflow_commitee_member";
+ $arr_all_files[]="workflow_employee";
+ $arr_all_files[]="workflow_file";
+ $arr_all_files[]="workflow_model";
+ $arr_all_files[]="workflow_orgunit";
+ $arr_all_files[]="workflow_role";
+ $arr_all_files[]="workflow_stage";
+ $arr_all_files[]="workflow_status";
+?>
