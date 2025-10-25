270e0ea3acccd72bb964eedab395151733a8badf
rafiq boubaker <rafik.towati@gmail.com>
2025-10-25 16:48:22 +0300

    work on nauss project

--- previleges.php
+++ previleges.php
@@ -703,50 +703,6 @@
       'id' => '105042',
     ),
   ),
-  'workflow_emp_request' => 
-  array (
-    'id' => '13986',
-    'display' => 
-    array (
-      'id' => -1,
-    ),
-    'search' => 
-    array (
-      'id' => -1,
-    ),
-    'qsearch' => 
-    array (
-      'id' => -1,
-    ),
-    'edit' => 
-    array (
-      'id' => -1,
-    ),
-    'qedit' => 
-    array (
-      'id' => -1,
-    ),
-    'crossed' => 
-    array (
-      'id' => -1,
-    ),
-    'stats' => 
-    array (
-      'id' => -1,
-    ),
-    'ddb' => 
-    array (
-      'id' => -1,
-    ),
-    'minibox' => 
-    array (
-      'id' => -1,
-    ),
-    'delete' => 
-    array (
-      'id' => -1,
-    ),
-  ),
   'workflow_file' => 
   array (
     'id' => '13936',
