53853598c7ca7269635c69908a02199d873b3412
raftow <rboubaker@tvtc.gov.sa>
2025-11-03 17:51:55 +0300

    work on adm-v2

--- all_to_pag.php
+++ all_to_pag.php
@@ -24,4 +24,24 @@ if($modtopag != "workflow") die("check the module you want to pag");
  $arr_all_files[]="workflow_role";
  $arr_all_files[]="workflow_stage";
  $arr_all_files[]="workflow_status";
+ $arr_all_files[]="content_element";
+ $arr_all_files[]="job_run";
+ $arr_all_files[]="job_run_result";
+ $arr_all_files[]="notification";
+ $arr_all_files[]="notification_schedule";
+ $arr_all_files[]="notification_template";
+ $arr_all_files[]="scjob";
+ $arr_all_files[]="workflow_action";
+ $arr_all_files[]="workflow_applicant";
+ $arr_all_files[]="workflow_commitee_scope";
+ $arr_all_files[]="workflow_entity";
+ $arr_all_files[]="workflow_event";
+ $arr_all_files[]="workflow_module";
+ $arr_all_files[]="workflow_orgunit";
+ $arr_all_files[]="workflow_rejection_reason";
+ $arr_all_files[]="workflow_request";
+ $arr_all_files[]="workflow_request_data";
+ $arr_all_files[]="workflow_scope";
+ $arr_all_files[]="workflow_task";
+ 
 ?>
