52471bcb011a5a01937c49565c9f9e1363617d91
rafiq boubaker <rafik.towati@gmail.com>
2025-12-16 10:06:55 +0300

    work on nauss project

--- tr/trad_ar_workflow_role.php
+++ tr/trad_ar_workflow_role.php
@@ -5,14 +5,14 @@ class WorkflowRoleArTranslator{
     {
         $trad = [];
 
-	$trad["workflow_role"]["workflowrole.single"] = "دور";
-	$trad["workflow_role"]["workflowrole.new"] = "جديد";
-	$trad["workflow_role"]["workflow_role"] = "أدوار";
-	$trad["workflow_role"]["role_category_enum"] = "فئة الدور";
-	$trad["workflow_role"]["role_name_ar"] = "اسم الدور - عربي";
-	$trad["workflow_role"]["role_name_en"] = "اسم الدور - انجليزي";
-	$trad["workflow_role"]["role_description_ar"] = "وصف الدور - عربي";
-	$trad["workflow_role"]["role_description_en"] = "وصف الدور - انجليزي";
+	$trad["workflow_role"]["workflowrole.single"] = "صلاحية";
+	$trad["workflow_role"]["workflowrole.new"] = "جديدة";
+	$trad["workflow_role"]["workflow_role"] = "الصلاحيات";
+	$trad["workflow_role"]["role_category_enum"] = "فئة الصلاحية";
+	$trad["workflow_role"]["role_name_ar"] = "اسم الصلاحية - عربي";
+	$trad["workflow_role"]["role_name_en"] = "اسم الصلاحية - انجليزي";
+	$trad["workflow_role"]["role_description_ar"] = "وصف الصلاحية - عربي";
+	$trad["workflow_role"]["role_description_en"] = "وصف الصلاحية - انجليزي";
         return $trad;
         }
 
