3932da53b2dbee7d04916ae1fcc1ddfe9e5d0f08
rafiq boubaker <rafik.towati@gmail.com>
2025-12-20 11:12:18 +0300

    work on nauss project

--- tr/trad_ar_workflow_transition.php
+++ tr/trad_ar_workflow_transition.php
@@ -13,13 +13,13 @@ class WorkflowTransitionArTranslator{
 		$trad["workflow_transition"]["workflow_module_id"] = "التطبيق";
 		$trad["workflow_transition"]["workflow_model_id"] = "نموذج سير عمل";
 		$trad["workflow_transition"]["initial_stage_id"] = "المرحلة الإبتدائية";
-		$trad["workflow_transition"]["initial_status_id"] = "حالة الطلب في البداية";
+		$trad["workflow_transition"]["initial_status_id"] = "الحالة الإبتدائية";
 		$trad["workflow_transition"]["workflow_action_id"] = "الإجراء";
 		$trad["workflow_transition"]["workflow_role_mfk"] = "الصلاحيات المطلوبة";
 		$trad["workflow_transition"]["workflow_condition_id"] = "شرط تمكين الاجراء";
 		$trad["workflow_transition"]["notification_template_id"] = "إشعار ما بعد التحول";
         $trad["workflow_transition"]["final_stage_id"] = "المرحلة النهائية";
-		$trad["workflow_transition"]["final_status_id"] = "حالة الطلب في النهاية";
+		$trad["workflow_transition"]["final_status_id"] = "الحالة النهائية";
 		
         // steps
         return $trad;
