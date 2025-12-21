3d77869b03db500e95ebfff6d398a784705777fe
rafiq boubaker <rafik.towati@gmail.com>
2025-12-21 22:17:02 +0300

    work on nauss project

--- models/workflow_stage.php
+++ models/workflow_stage.php
@@ -45,9 +45,14 @@
                         foreach($workflowStatusList as $workflowStatusObj)
                         {
                                 $myWorkflowActionList = $workflowActionList[$workflowStatusObj->id];                                
-                                if((($this->id==1) and ($workflowStatusObj->id==22)) or (!$myWorkflowActionList) or (!is_array($myWorkflowActionList)) or (count($myWorkflowActionList)==0))
+                                if((($this->id==1) and ($workflowStatusObj->id==22)))
                                 {
-                                        die("workflowActionList = ".var_export($workflowActionList, true)." myWorkflowActionList[$workflowStatusObj->id] = ".var_export($myWorkflowActionList, true));
+                                        die("stage $this->id workflowActionList = ".var_export($workflowActionList, true)." myWorkflowActionList[$workflowStatusObj->id] = ".var_export($myWorkflowActionList, true));
+                                }
+
+                                if((!$myWorkflowActionList) or (!is_array($myWorkflowActionList)) or (count($myWorkflowActionList)==0))
+                                {
+                                        die("stage $this->id workflowActionList = ".var_export($workflowActionList, true)." myWorkflowActionList[$workflowStatusObj->id] = ".var_export($myWorkflowActionList, true));
                                 }
                                 /**
                                  * @var WorkflowStatus $workflowStatusObj
