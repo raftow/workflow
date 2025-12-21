fb2b950e3d1484819a121ed1a399e35d0547d1dc
rafiq boubaker <rafik.towati@gmail.com>
2025-12-21 08:52:14 +0300

    work on nauss project

--- home.php
+++ home.php
@@ -71,13 +71,13 @@ $out_scr .= "<script type=\"text/javascript\">
         function positioningBlockBug() {
             var oldNode = window.treemain.nodeById(2);
             //var newNode = $('#node_2_new');
-            var newNode = $('    <div id=\"node_2\" class=\"window hidden\"\n' +
-                '         data-id=\"2\"\n' +
-                '         data-parent=\"0\"\n' +
-                '         data-first-child=\"6\"\n' +
-                '         data-next-sibling=\"3\">\n' +
-                '        Node 2 NEW\n' +
-                '    </div>\n');
+            var newNode = $('    <div id=\"node_2\" class=\"window hidden\"\\n' +
+                '         data-id=\"2\"\\n' +
+                '         data-parent=\"0\"\\n' +
+                '         data-first-child=\"6\"\\n' +
+                '         data-next-sibling=\"3\">\\n' +
+                '        Node 2 NEW\\n' +
+                '    </div>\\n');
             if (oldNode) {
                 // butta il nodo nel container
                 oldNode.replaceWith(newNode);
