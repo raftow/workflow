f936808b6dae94432e60bcd1e828532aa3c5f036
rafiq boubaker <rafik.towati@gmail.com>
2026-01-03 09:48:38 +0300

    work on nauss project

--- api/wkfaddcomment.php
+++ api/wkfaddcomment.php
@@ -1,7 +1,8 @@
 <?php
 
 $file_dir_name = dirname(__FILE__);
-require_once ('../afw/afw_autoloader.php');
+require_once ("$file_dir_name/../../lib/afw/afw_autoloader.php");
+include_once ("$file_dir_name/../../lib/afw/afw_error_handler.php");
 set_time_limit(8400);
 ini_set('error_reporting', E_ERROR | E_PARSE | E_RECOVERABLE_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR);
 $lang = 'en';
