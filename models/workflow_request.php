b22dafeef7105185be0fb4bea3dd11c867ac47b2
rafiq boubaker <rafik.towati@gmail.com>
2026-01-03 11:39:33 +0300

    work on nauss project

--- models/workflow_request.php
+++ models/workflow_request.php
@@ -341,30 +341,30 @@ class WorkflowRequest extends WorkflowObject
                                         let rowComment = comment;
 
                                         // Construct the new row's HTML
-                                        let newRowHtml = `
-                                        <tr id=\"tr-object-\${rowId}\" class=\"ky item workf csr_active hzm_row_Y\">
-                                                <td id='comment_date-\${rowId}' class='col-importance-high hzm_col hzm_col_afw hzm_col_afw_comme hzm_col_afw_comment_date'>
-                                                        <span class='comment_date-span'>\$rowDate
+                                        let newRowHtml = \"
+                                        <tr id='tr-object-\"+id+\"' class='ky item workf csr_active hzm_row_Y'>
+                                                <td id='comment_date-\"+id+\"' class='col-importance-high hzm_col hzm_col_afw hzm_col_afw_comme hzm_col_afw_comment_date'>
+                                                        <span class='comment_date-span'>\"+dd+\"
                                                         </span></td>        
-                                                <td id='request_comment_subject_id-\${rowId}' class='col-importance-high hzm_col hzm_col_afw hzm_col_afw_reque hzm_col_afw_request_comment_subject_id'>
-                                                        <span class='request_comment_subject_id-span'>\$rowSubject
+                                                <td id='request_comment_subject_id-\"+id+\"' class='col-importance-high hzm_col hzm_col_afw hzm_col_afw_reque hzm_col_afw_request_comment_subject_id'>
+                                                        <span class='request_comment_subject_id-span'>\"+subject+\"
                                                         </span></td>        
-                                                <td id='workflow_stage_id-\${rowId}' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_workf hzm_col_afw_workflow_stage_id'>
-                                                        <span class='workflow_stage_id-span'>\$rowStage
+                                                <td id='workflow_stage_id-\"+id+\"' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_workf hzm_col_afw_workflow_stage_id'>
+                                                        <span class='workflow_stage_id-span'>\"+stage+\"
                                                         </span></td>        
-                                                <td id='comment-\${rowId}' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_comme hzm_col_afw_comment'>
-                                                        <span class='comment-span'>\$rowComment
+                                                <td id='comment-\"+id+\"' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_comme hzm_col_afw_comment'>
+                                                        <span class='comment-span'>\"+comment+\"
                                                         </span></td>        
-                                                <td id='عرض-\${rowId}' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_3Ru hzm_col_afw_3Ru'>
+                                                <td id='عرض-\"+id+\"' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_3Ru hzm_col_afw_3Ru'>
                                                         <span class='عرض-span'>
-                                                        <a href='main.php?Main_Page=afw_mode_display.php&amp;cl=WorkflowRequestComment&amp;currmod=workflow&amp;id=\${rowId}&amp;currstep=1' data-original-title='' title=''>
+                                                        <a href='main.php?Main_Page=afw_mode_display.php&amp;cl=WorkflowRequestComment&amp;currmod=workflow&amp;id=\"+id+\"&amp;currstep=1' data-original-title='' title=''>
                                                                 <img src='../lib/images/view_ok.png' width='24' heigth='24' data-toggle='tooltip' data-placement='top' title='' data-original-title=''></a>
                                                         </span></td>        
-                                                <td id='مسح-\${rowId}' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_MS7 hzm_col_afw_MS7'>
-                                                        <a href='#' here='afw_shwr' id='\${rowId}' cl='WorkflowRequestComment' md='workflow' lbl='' lvl='2' class='trash showmany' data-original-title='' title=''>
+                                                <td id='مسح-\"+id+\"' class='col-importance-small hzm_col hzm_col_afw hzm_col_afw_MS7 hzm_col_afw_MS7'>
+                                                        <a href='#' here='afw_shwr' id='\"+id+\"' cl='WorkflowRequestComment' md='workflow' lbl='' lvl='2' class='trash showmany' data-original-title='' title=''>
                                                         <img src='../lib/images/trash.png' style='height: 22px !important;'></a></td>  
                                         </tr>
-                                        `;
+                                        \";
                                         
                                         // Append the new row to the tbody
                                         \$(\"#workflowRequestCommentListTableBody\").append(newRowHtml);
