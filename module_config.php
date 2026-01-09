76e94ccba43761a7fe692172280dbb3f54f21bb7
rafiq boubaker <rafik.towati@gmail.com>
2026-01-09 15:40:44 +0300

    work on nauss project

--- module_config.php
+++ module_config.php
@@ -1,29 +1,28 @@
 <?php
-    // will be deprecated and then obsoleted
-    $TECH_FIELDS = array();
-    $MODULE = "workflow";
-    $THIS_MODULE_ID = 1283;
-    $MODULE_FRAMEWORK[$THIS_MODULE_ID] = 1;                
-    $LANGS_MODULE = array("ar"=>true,"fr"=>false,"en"=>true);
-    
-    $file_box_css_class = "rea_filebox";  // by default filebox
-    
-    $file_type_ids = "1,9,10,12,13,17,18,27";
-    
-    $module_config_token = array();
-    $module_config_token["file_types"] = $file_type_ids;
-    
-    // $config["img-path"] = "pic/";
-    // // $config["img-company-path"] = "../exte-rnal/pic/";
-    
-    $display_in_edit_mode["*"] = true;
+// will be deprecated and then obsoleted
+$TECH_FIELDS = array();
+$MODULE = "workflow";
+$THIS_MODULE_ID = 1283;
+$MODULE_FRAMEWORK[$THIS_MODULE_ID] = 1;
+$LANGS_MODULE = array("ar" => true, "fr" => false, "en" => true);
 
-    $TECH_FIELDS[$MODULE]["CREATION_USER_ID_FIELD"]="created_by";
-    $TECH_FIELDS[$MODULE]["CREATION_DATE_FIELD"]="created_at";
-    $TECH_FIELDS[$MODULE]["UPDATE_USER_ID_FIELD"]="updated_by";
-    $TECH_FIELDS[$MODULE]["UPDATE_DATE_FIELD"]="updated_at";
-    $TECH_FIELDS[$MODULE]["VALIDATION_USER_ID_FIELD"]="validated_by";
-    $TECH_FIELDS[$MODULE]["VALIDATION_DATE_FIELD"]="validated_at";
-    $TECH_FIELDS[$MODULE]["VERSION_FIELD"]="version";
-    $TECH_FIELDS[$MODULE]["ACTIVE_FIELD"]="active";
-?>
+$file_box_css_class = "rea_filebox";  // by default filebox
+
+$file_type_ids = "6,7,18,19,20,27,28,29,33,34";
+
+$module_config_token = array();
+$module_config_token["file_types"] = $file_type_ids;
+
+// $config["img-path"] = "pic/";
+// // $config["img-company-path"] = "../exte-rnal/pic/";
+
+$display_in_edit_mode["*"] = true;
+
+$TECH_FIELDS[$MODULE]["CREATION_USER_ID_FIELD"] = "created_by";
+$TECH_FIELDS[$MODULE]["CREATION_DATE_FIELD"] = "created_at";
+$TECH_FIELDS[$MODULE]["UPDATE_USER_ID_FIELD"] = "updated_by";
+$TECH_FIELDS[$MODULE]["UPDATE_DATE_FIELD"] = "updated_at";
+$TECH_FIELDS[$MODULE]["VALIDATION_USER_ID_FIELD"] = "validated_by";
+$TECH_FIELDS[$MODULE]["VALIDATION_DATE_FIELD"] = "validated_at";
+$TECH_FIELDS[$MODULE]["VERSION_FIELD"] = "version";
+$TECH_FIELDS[$MODULE]["ACTIVE_FIELD"] = "active";
