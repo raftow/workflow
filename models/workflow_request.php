a36e66ff033c2975c452c420bc5006a36697d738
rafiq boubaker <rafik.towati@gmail.com>
2026-02-04 22:07:37 +0300

    work on nauss project

--- models/workflow_request.php
+++ models/workflow_request.php
@@ -1,4 +1,11 @@
 <?php
+// RB : 04 FEB 2026 : cleaning bad data
+// -- select applicant_id from  nauss_adm.application_desire where application_plan_branch_id not in (select id from nauss_adm.application_plan_branch)
+// delete from nauss_workflow.workflow_request where workflow_applicant_id in ('1091929602','3000000006','1013585300');
+// delete from  nauss_adm.application_desire where application_plan_branch_id not in (select id from nauss_adm.application_plan_branch);
+
+
+
 class WorkflowRequest extends WorkflowObject
 {
         public static $DATABASE = '';
@@ -29,7 +36,7 @@ class WorkflowRequest extends WorkflowObject
                                 0 => array("COLUMN" => "workflow_scope_id", "DISPLAY-FORMAT" => "decode", "FOOTER_SUM_TITLE" => "الإجمــالـي"),
                                 1 => array("COLUMN" => "workflow_status_id", "DISPLAY-FORMAT" => "decode", "FOOTER_SUM_TITLE" => "الإجمــالـي"),
                         ),
-                        "CROSS_STATS_COLS" => ["row" => "workflow_scope_id", "col" => "workflow_status_id", "val" => 'count_request', 'bigcol'=>'workflow_stage_id'],
+                        "CROSS_STATS_COLS" => ["row" => "workflow_scope_id", "col" => "workflow_status_id", "val" => 'count_request', 'bigcol' => 'workflow_stage_id'],
 
                         "DISPLAY_COLS" => array(
                                 "count_request" => array("COLUMN" => "count_request", "SQL_FORMULA" => "count(id)", "SHOW-NAME" => "count_request", "ROW_SUM" => true, "COL_SUM" => true),
@@ -1325,22 +1332,21 @@ class WorkflowRequest extends WorkflowObject
 
         public function statsColCategory($col, $stats_code)
         {
-                if($stats_code=='wr0001')
-                {
+                if ($stats_code == 'wr0001') {
                         if (AfwStringHelper::stringStartsWith($col, "cross_col")) {
-                                $workflow_status_id = substr($col,10);
+                                $workflow_status_id = substr($col, 10);
                                 list($stageObj, $color) = WorkflowStatus::colorOfStatus($workflow_status_id);
                                 return "stats_cross_col stats_$color";
-                        }
-                        else return 'stats_cross_row';
+                        } else return 'stats_cross_row';
                 }
                 return parent::statsColCategory($col, $stats_code);
         }
 
-        public function colorOf($attribute, $attribute_value) {
-                if($attribute=="workflow_stage_id") {
-                        $obj = WorkflowStage::loadById($attribute_value); 
-                        if($obj) return self::code_of_color_enum($obj->getVal("color_enum"));
+        public function colorOf($attribute, $attribute_value)
+        {
+                if ($attribute == "workflow_stage_id") {
+                        $obj = WorkflowStage::loadById($attribute_value);
+                        if ($obj) return self::code_of_color_enum($obj->getVal("color_enum"));
                         else return "black";
                 }
         }
