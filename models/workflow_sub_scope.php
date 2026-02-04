f5ae2caf4a145dc1c129a58d1f67c2bdf47f2a5a
rafiq boubaker <rafik.towati@gmail.com>
2026-02-04 07:30:40 +0300

    work on nauss project

---
+++ models/workflow_sub_scope.php
@@ -0,0 +1,103 @@
+<?php
+
+class WorkflowSubScope extends WorkflowObject
+{
+
+    public static $DATABASE = '';
+
+    public static $MODULE = 'workflow';
+
+    public static $TABLE = 'workflow_sub_scope';
+
+    public static $DB_STRUCTURE = null;
+    // public static $copypast = true;
+
+    public function __construct()
+    {
+        parent::__construct('workflow_sub_scope', 'id', 'workflow');
+        WorkflowWorkflowSubScopeAfwStructure::initInstance($this);
+    }
+
+    public static function loadById($id)
+    {
+        $obj = new WorkflowSubScope();
+
+        if ($obj->load($id)) {
+            return $obj;
+        } else {
+            return null;
+        }
+    }
+
+    public static function loadByMainIndex($workflow_module_id, $lookup_code, $create_obj_if_not_found = false)
+    {
+        if (! $workflow_module_id) {
+            throw new AfwRuntimeException('loadByMainIndex : workflow_module_id is mandatory field');
+        }
+
+        if (! $lookup_code) {
+            throw new AfwRuntimeException('loadByMainIndex : lookup_code is mandatory field');
+        }
+
+        $obj = new WorkflowSubScope();
+        $obj->select('workflow_module_id', $workflow_module_id);
+        $obj->select('lookup_code', $lookup_code);
+
+        if ($obj->load()) {
+            if ($create_obj_if_not_found) {
+                $obj->activate();
+            }
+
+            return $obj;
+        } elseif ($create_obj_if_not_found) {
+            $obj->set('workflow_module_id', $workflow_module_id);
+            $obj->set('lookup_code', $lookup_code);
+
+            $obj->insertNew();
+            if (! $obj->id) {
+                return null;
+            }
+
+            // means beforeInsert rejected insert operation
+            $obj->is_new = true;
+            return $obj;
+        } else {
+            return null;
+        }
+    }
+
+    public function getDisplay($lang = 'ar')
+    {
+        return $this->getDefaultDisplay($lang);
+    }
+
+    public function stepsAreOrdered()
+    {
+        return false;
+    }
+
+
+    public function select_visibilite_horizontale($dropdown = false)
+    {
+        $objme = AfwSession::getUserConnected();
+
+        if ($objme and $objme->isAdmin()) {
+            // no VH for system admin
+        }
+        /* Amjad whatsapp 18/01/2026 7pm ~ audio say do not limit for users for the moment
+        else {
+            $employee_id = $objme ? $objme->getEmployeeId() : 0;
+
+            if ($employee_id) {
+                $wEmplObj = WorkflowEmployee::findWorkflowEmployee($employee_id);
+                if ($wEmplObj) {
+                    $wscope_mfk = trim($wEmplObj->getVal("wscope_mfk"), ",");
+                    $this->where("id in ($wscope_mfk)");
+                }
+            }
+        }*/
+
+        $selects = array();
+        $this->select_visibilite_horizontale_default($dropdown, $selects);
+    }
+}
---
+++ tr/trad_ar_workflow_sub_scope.php
@@ -0,0 +1,26 @@
+<?php
+class WorkflowSubScopeArTranslator
+{
+
+	public static function initData()
+	{
+		$trad = [];
+
+		$trad["workflow_sub_scope"]["workflowsubscope.single"] = "فرع";
+		$trad["workflow_sub_scope"]["workflowsubscope.new"] = "جديد	";
+		$trad["workflow_sub_scope"]["workflow_sub_scope"] = "البرامج";
+		$trad["workflow_sub_scope"]["sub_scope_name_ar"] = "اسم الفرع - عربي";
+		$trad["workflow_sub_scope"]["sub_scope_name_en"] = "اسم الفرع - انجليزي";
+		$trad["workflow_sub_scope"]["sub_scope_description_ar"] = "وصف الفرع - عربي";
+		$trad["workflow_sub_scope"]["sub_scope_description_en"] = "وصف الفرع - انجليزي";
+		$trad["workflow_sub_scope"]["workflow_module_id"] = "وحدة العمل";
+		$trad["workflow_sub_scope"]["workflow_scope_id"] = "البرنامج";
+		return $trad;
+	}
+
+	public static function getInstance()
+	{
+		if (false) return new WorkflowSubScopeEnTranslator();
+		return new WorkflowSubScope();
+	}
+}
