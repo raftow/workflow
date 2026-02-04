074d6c28bfb96091ebf78ace52fd796c7399b617
rafiq boubaker <rafik.towati@gmail.com>
2026-02-04 07:33:43 +0300

    work on nauss project

--- tr/trad_ar_workflow_sub_scope.php
+++ tr/trad_ar_workflow_sub_scope.php
@@ -8,7 +8,7 @@ class WorkflowSubScopeArTranslator
 
 		$trad["workflow_sub_scope"]["workflowsubscope.single"] = "فرع";
 		$trad["workflow_sub_scope"]["workflowsubscope.new"] = "جديد	";
-		$trad["workflow_sub_scope"]["workflow_sub_scope"] = "البرامج";
+		$trad["workflow_sub_scope"]["workflow_sub_scope"] = "الفروع";
 		$trad["workflow_sub_scope"]["sub_scope_name_ar"] = "اسم الفرع - عربي";
 		$trad["workflow_sub_scope"]["sub_scope_name_en"] = "اسم الفرع - انجليزي";
 		$trad["workflow_sub_scope"]["sub_scope_description_ar"] = "وصف الفرع - عربي";
