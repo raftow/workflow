c1589d9d64bfd26c9b140ecfdf2eeaad9a0e68d0
rafiq boubaker <rafik.towati@gmail.com>
2026-02-06 21:49:18 +0300

    work on nauss project

--- struct/workflow_request_afw_structure.php
+++ struct/workflow_request_afw_structure.php
@@ -19,6 +19,7 @@ class WorkflowWorkflowRequestAfwStructure
 
                         $obj->editByStep = true;
                         $obj->editNbSteps = 8;
+                        $obj->excelExport = true;
                         // $obj->after_save_edit = array("class"=>'aconditionOriginType',"attribute"=>'acondition_origin_type_id', "currmod"=>'workflow',"currstep"=>1);
                         // $obj->after_save_edit = array('mode' => 'qsearch', 'currmod' => 'Workflow', 'class' => 'WorkflowRequest', 'submit' => true);
                         $obj->after_save_edit = array("file" => '../workflow/inbox.php');
