24d700fb4ac11dc51fd8163c5b2a1b832a5b8ffb
rafiq boubaker <rafik.towati@gmail.com>
2026-02-11 15:15:38 +0300

    work on nauss project

--- previleges/role/previleges_workflow_role403_special.php
+++ previleges/role/previleges_workflow_role403_special.php
@@ -1,11 +1,33 @@
 <?php
 $role_info[403]['menu']['items'][10000000001] = array(
     'id' => '10000000001',
-    'code' => 'inbox',
+    'code' => 'comparison',
     'level' => '100',
-    'menu_name_ar' => 'شاشة المفاضلة والقبول المبدئي',
+    'menu_name_ar' => 'المفاضلة والقبول المبدئي',
     'menu_name_en' => 'Preliminary admission and comparison screen',
     'page' => 'main.php?Main_Page=wrsorting.php',
+    'css' => 'bf',
+    'icon' => 'bficon-std bfc-',
+);
+
+$role_info[403]['menu']['items'][10000000002] = array(
+    'id' => '10000000002',
+    'code' => 'execution',
+    'level' => '100',
+    'menu_name_ar' => 'إجراءات تنفيذ القبول',
+    'menu_name_en' => 'Execution of admission procedures',
+    'page' => 'main.php?Main_Page=wrsorting.php&option=exec',
+    'css' => 'bf',
+    'icon' => 'bficon-std bfc-',
+);
+
+$role_info[403]['menu']['items'][10000000003] = array(
+    'id' => '10000000003',
+    'code' => 'interview',
+    'level' => '100',
+    'menu_name_ar' => 'متابعة المقابلات',
+    'menu_name_en' => 'Interview follow-up',
+    'page' => 'main.php?Main_Page=interviewApplicant.php',
     'css' => 'bf',
     'icon' => 'bficon-std bfc-',
 );
