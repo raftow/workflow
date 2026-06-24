f007b903fc5a6f0279edde878ab757f357101c47
rafiq boubaker <rafik.towati@gmail.com>
2026-06-25 00:29:48 +0300

    some important improvement

--- all_to_pag.php
+++ all_to_pag.php
@@ -1,49 +1,50 @@
 <?php
 
-if($modtopag != "workflow") die("check the module you want to pag");
+/**
+ * @var string $modtopag
+ */
+if ($modtopag != "workflow") die("check the module you want to pag");
 
- $arr_all_files = array();
+$arr_all_files = array();
 
- 
- $arr_all_files[]="workflow_entity";
- $arr_all_files[]="workflow_module";
- $arr_all_files[]="content";
- $arr_all_files[]="content_item";
- $arr_all_files[]="idn_to_id";
- $arr_all_files[]="intelligent_content";
- $arr_all_files[]="page";
- $arr_all_files[]="page_item";
- $arr_all_files[]="page_section";
- $arr_all_files[]="page_theme";
- $arr_all_files[]="publication";
- $arr_all_files[]="section_template";
- $arr_all_files[]="workflow_commitee";
- $arr_all_files[]="workflow_commitee_member";
- $arr_all_files[]="workflow_employee";
- $arr_all_files[]="workflow_file";
- $arr_all_files[]="workflow_model";
- $arr_all_files[]="workflow_orgunit";
- $arr_all_files[]="workflow_role";
- $arr_all_files[]="workflow_stage";
- $arr_all_files[]="workflow_status";
- $arr_all_files[]="job_run";
- $arr_all_files[]="job_run_result";
- $arr_all_files[]="notification_template";
- $arr_all_files[]="workflow_event";
- $arr_all_files[]="notification";
- $arr_all_files[]="notification_schedule";
- 
- $arr_all_files[]="scjob";
- $arr_all_files[]="workflow_action";
- $arr_all_files[]="workflow_applicant_file";
- $arr_all_files[]="workflow_applicant";
- $arr_all_files[]="workflow_commitee_scope";
- 
- $arr_all_files[]="workflow_orgunit";
- $arr_all_files[]="workflow_rejection_reason";
- $arr_all_files[]="workflow_request";
- $arr_all_files[]="workflow_request_data";
- $arr_all_files[]="workflow_scope";
- $arr_all_files[]="workflow_task";
- 
-?>
+
+$arr_all_files[] = "workflow_entity";
+$arr_all_files[] = "workflow_module";
+$arr_all_files[] = "content";
+$arr_all_files[] = "content_item";
+$arr_all_files[] = "idn_to_id";
+$arr_all_files[] = "intelligent_content";
+$arr_all_files[] = "page";
+$arr_all_files[] = "page_item";
+$arr_all_files[] = "page_section";
+$arr_all_files[] = "page_theme";
+$arr_all_files[] = "publication";
+$arr_all_files[] = "section_template";
+$arr_all_files[] = "workflow_commitee";
+$arr_all_files[] = "workflow_commitee_member";
+$arr_all_files[] = "workflow_employee";
+$arr_all_files[] = "workflow_file";
+$arr_all_files[] = "workflow_model";
+$arr_all_files[] = "workflow_orgunit";
+$arr_all_files[] = "workflow_role";
+$arr_all_files[] = "workflow_stage";
+$arr_all_files[] = "workflow_status";
+$arr_all_files[] = "job_run";
+$arr_all_files[] = "job_run_result";
+$arr_all_files[] = "notification_template";
+$arr_all_files[] = "workflow_event";
+$arr_all_files[] = "notification";
+$arr_all_files[] = "notification_schedule";
+
+$arr_all_files[] = "scjob";
+$arr_all_files[] = "workflow_action";
+$arr_all_files[] = "workflow_applicant_file";
+$arr_all_files[] = "workflow_applicant";
+$arr_all_files[] = "workflow_commitee_scope";
+
+$arr_all_files[] = "workflow_orgunit";
+$arr_all_files[] = "workflow_rejection_reason";
+$arr_all_files[] = "workflow_request";
+$arr_all_files[] = "workflow_request_data";
+$arr_all_files[] = "workflow_scope";
+$arr_all_files[] = "workflow_task";
